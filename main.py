2c79ef078453ed9a1bb657cd8cd3e260582addc1
martijn <martijndenhoed17@gmail.com>
2021-11-09 16:40:43 +0100

    yoink

--- main.py
+++ main.py
@@ -7,6 +7,34 @@
 
 print("bruhh")
 
+
+##all SI units please
+g = 9.80665
+
+
+RTGmass = 52.8
+RTGlength = 1.07
+RTGdiameter = 0.47
+
+GStaticlong = 4.55
+GStaticLat = 0.25
+
+GDynamicLong = 1.0
+GDynamicLat = 0.8
+
+SafetyFactorLaunch = 1.25
+
+GLongMax = (GStaticlong + GDynamicLong) * SafetyFactorLaunch
+GlatMax = (GStaticLat + GDynamicLat) * SafetyFactorLaunch
+
+PLong = GLongMax * RTGmass
+Plat = GlatMax * RTGmass
+
+class material:
+    def __init__(self,E,sigmaYield):
+
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
